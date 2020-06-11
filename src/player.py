da893f3fb0f8a09eed7e5250a61d0e9ca0b7e3bf
Dennis Padiernos <depadiernos@gmail.com>
2020-06-11 18:38:45 -0500

    Create base Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,24 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_location):
+        self.name = name
+        self.current_location = current_location
+    def __str__(self):
+        print(f'This is {self.name}. They are currently standing {location}')
+    
+    def move_to(self, direction):
+        if direction == 'n' and self.current_location.n_to != None:
+            self.current_location = self.current_location.n_to
+            return True
+        if direction == 'e' and self.current_location.e_to != None:
+            self.current_location = self.current_location.e_to
+            return True
+        if direction == 's' and self.current_location.s_to != None:
+            self.current_location = self.current_location.s_to
+            return True
+        if direction == 'w' and self.current_location.w_to != None:
+            self.current_location = self.current_location.w_to
+            return True
+        return False
