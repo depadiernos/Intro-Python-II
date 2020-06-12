f79a20d5c2fcc932add10ea5080945eec2994438
Dennis Padiernos <depadiernos@gmail.com>
2020-06-11 19:26:49 -0500

    split initialization to another file.

--- src/adv.py
+++ src/adv.py
@@ -1,36 +1,4 @@
-from room import Room
-from player import Player
-
-# Declare all the rooms
-
-
-outside = Room("Outside Cave Entrance", "North of you, the cave mount beckons")
-
-foyer = Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""")
-
-overlook = Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""")
-
-narrow =   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""")
-
-treasure = Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""")
-
-
-# Link rooms together
-
-outside.n_to = foyer
-foyer.s_to = outside
-foyer.n_to = overlook
-foyer.e_to = narrow
-overlook.s_to = foyer
-narrow.w_to = foyer
-narrow.n_to = treasure
-treasure.s_to = narrow
+import init
 
 #
 # Main
@@ -52,7 +20,7 @@ treasure.s_to = narrow
 def mud():
     directions = ['n', 'e', 's', 'w']
     name = input("What's your name? ")
-    player = Player(name, outside)
+    player = init.player(name)
 
     print(f'Hi {name}!\nYou can move around by typing (n)orth, (e)ast, (s)outh, (w)est. Or type (q) to exit')
 
